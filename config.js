17f91ecea0e0a9f7734c3de0c7b207d54532eb9a
Richard Walter III <RichardWalter5592@users.noreply.github.com>
2020-04-10 08:21:37 -0500

    Final Push

---
+++ config.js
@@ -0,0 +1,2 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiY2hhcmR0aGUzcmQiLCJhIjoiY2s4dTdsbmRoMDZhOTNobnJ3OHlncTZiZiJ9.qQT2eckqizg74ooGw0v6Hw";
--- static/js/config.js
+++
@@ -1,2 +0,0 @@
-// API key
-const API_KEY = "pk.eyJ1IjoiY2hhcmR0aGUzcmQiLCJhIjoiY2s4dGdldTFyMDAwazNmcHJlaWl3ZWx1NSJ9.BPyUOcP6UKHLXuklH-zsNQ";
